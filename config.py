cda21dbac5f4f0b78bdc80ce74cbf4f477c3c4f4
Matthew Lewis <matt@mplewis.com>
2014-07-23 00:05:04 -0700

    Implement basic config

---
+++ config.py
@@ -0,0 +1,9 @@
+class Flask:
+    DEBUG = True
+    HOST = '0.0.0.0'
+
+
+class Metadata:
+    NAME = 'Verilive Server'
+    VERSION = (0, 0, 1)
+    CONTACT = 'info@verilog.me'
--- server.py
+++ server.py
@@ -1,12 +1,33 @@
 import sys
 
+from flask import Flask, jsonify
+
+import config
+
+
+app = Flask(__name__)
+app.config.from_object(config.Flask)
+
+
+@app.route('/')
+def about():
+    version = '.'.join([str(x) for x in config.Metadata.VERSION])
+    return jsonify(name=config.Metadata.NAME,
+                   version=version,
+                   contact=config.Metadata.CONTACT)
+
+
+@app.route('/compile', methods=['POST'])
+def compile():
+    pass
+
 
 def main():
     if sys.version_info[0] != 3:
         print("This is a Python 3 script. You are running Python %s.%s.%s." %
               sys.version_info[:3])
         sys.exit(1)
-    print('OK')
+    app.run()
 
 
 if __name__ == '__main__':
